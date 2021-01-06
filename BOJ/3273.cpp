9a0d48d443db70327bdf5db8f565fc521a97d1c4
YooByungjo <ubj0314@naver.com>
2021-01-06 20:25:41 +0900

    3273번 두 수의 합

---
+++ BOJ/3273.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(),x.end()
+#define y1 omg
+#define INF 1000000010
+#define FOR(x, n) for(int x=0; x<n; x++)
+#define ini(x, n) memset(x, n, sizeof(x));
+#define MOD ((int)1e9 + 7)
+#define atoi(x) ((x)-'0')
+#define int ll
+//#define double long double
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;	
+typedef vector<vi> vvi;
+
+int dx[] = { -2, -2, -1, -1,  1, 1,  2, 2 };
+int dy[] = { -1,  1, -2,  2, -2, 2, -1, 1 };
+
+int n, x, t, ans;
+map<int, int> M;
+
+signed main() {
+	fastio;
+	cin >> n;
+	FOR(i, n) {
+		cin >> t;
+		M[t]++;
+	}
+	cin >> x;
+	for (auto [k, v] : M) {
+		if (M.find(x - k) != M.end())
+			ans += v * M[x - k];
+	}
+	cout << ans / 2;
+}
