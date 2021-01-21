6913527cb00fa6619b3ca104ef3355465d3bfb13
YooByungjo <ubj0314@naver.com>
2021-01-21 19:18:02 +0900

    10539번 수빈이와 수열

---
+++ BOJ/10539.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(),x.end()
+#define y1 omg
+#define INF 1000000010
+#define FOR(x, n) for(int x=0; x<n; x++)
+#define ini(x, n) memset(x, n, sizeof(x));
+#define MOD ((int)1e9 + 7)
+#define atoi(x) ((x)-'0')
+#define int ll
+//#define double long double
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+int dx[] = { -2, -2, -1, -1,  1, 1,  2, 2 };
+int dy[] = { -1,  1, -2,  2, -2, 2, -1, 1 };
+
+int n, cur;
+
+signed main() {
+	fastio;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		int t; cin >> t;
+		cout << t * i - cur << " ";
+		cur += t * i - cur;
+	}
+}
