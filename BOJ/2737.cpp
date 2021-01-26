5dc58a32def39f63243352e668dbce1a86461282
YooByungjo <ubj0314@naver.com>
2021-01-26 20:29:11 +0900

    2737번 연속 합

---
+++ BOJ/2737.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(),x.end()
+#define y1 omg
+#define INF 1000000010
+#define FOR(x, n) for(int x=0; x<n; x++)
+#define ini(x, n) memset(x, n, sizeof(x));
+#define MOD ((int)1e9 + 7)
+#define atoi(x) ((x)-'0')
+#define int ll
+//#define double long double
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+int dx[] = { -2, -2, -1, -1,  1, 1,  2, 2 };
+int dy[] = { -1,  1, -2,  2, -2, 2, -1, 1 };
+
+int TC, m;
+
+signed main() {
+	cin >> TC;
+	while (TC--) {
+		cin >> m;
+		int ans = 0;
+		for (int n = 2;; n++) {
+			int minSum = (n * (n + 1)) / 2;
+			if (m < minSum) break;
+			else if ((m - minSum) % n == 0) ans++;
+		}
+		cout << ans << endl;
+	}
+}
