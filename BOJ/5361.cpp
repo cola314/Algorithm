1e615a883be13719796e5d3731308ba8fdd82b26
YooByungjo <ubj0314@naver.com>
2021-01-27 23:44:20 +0900

    5361번 전투 드로이드 가격

---
+++ BOJ/5361.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(),x.end()
+#define y1 omg
+#define INF 1000000010
+#define FOR(x, n) for(int x=0; x<n; x++)
+#define ini(x, n) memset(x, n, sizeof(x));
+#define MOD ((int)1e9 + 7)
+#define atoi(x) ((x)-'0')
+#define int ll
+#define double long double
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+int dx[] = { -2, -2, -1, -1,  1, 1,  2, 2 };
+int dy[] = { -1,  1, -2,  2, -2, 2, -1, 1 };
+
+int d[] = { 35034, 23090, 19055, 12530, 18090 }, TC, t;
+
+signed main() {
+	cin >> TC;
+	while (TC--) {
+		int ans = 0;
+		FOR(i, 5) {
+			cin >> t;
+			ans += t * d[i];
+		}
+		printf("$%lld.%02lld\n", ans / 100, ans % 100);
+	}
+}
