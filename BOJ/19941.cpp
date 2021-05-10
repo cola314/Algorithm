fc45b70dec4d6cf641aee397752bd8daa156d418
yoobyungjo <ubj0314@naver.com>
2021-05-10 22:20:34 +0900

    [2020 KOI 중등 1차] 햄버거 분배

---
+++ BOJ/19941.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(),x.end()
+#define y1 omg
+#define INF 1000000010
+#define FOR(x, n) for(int x=0; x<n; x++)
+#define ini(x, n) memset(x, n, sizeof(x));
+#define MOD ((int)1e9 + 7)
+#define atoi(x) ((x)-'0')
+#define int ll
+#define double long double
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+int dx[] = { -2, -2, -1, -1,  1, 1,  2, 2 };
+int dy[] = { -1,  1, -2,  2, -2, 2, -1, 1 };
+
+int n, k, ans;
+string s;
+
+signed main() {
+	cin >> n >> k >> s;
+	FOR(i, sz(s)) {
+		if (s[i] == 'P') {
+			for (int j = i - k; j <= i + k; j++) {
+				if (0 <= j && j < sz(s) && s[j] == 'H') {
+					s[j] = 0;
+					ans++;
+					break;
+				}
+			}
+		}
+	}
+	cout << ans;
+}
