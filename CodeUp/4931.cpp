7585af8ebc1ee0e378c89fb401fe917c291cfb05
yoobyungjo <ubj0314@naver.com>
2021-05-22 20:06:57 +0900

    [2020 KOI 초등 1차] 박 터뜨리기

---
+++ CodeUp/4931.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define endl "\n"
+#define fastio ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define all(x) x.begin(),x.end()
+#define y1 omg
+#define INF 1000000010
+#define FOR(x, n) for(int x=0; x<n; x++)
+#define ini(x, n) memset(x, n, sizeof(x));
+#define MOD ((int)1e9 + 7)
+#define atoi(x) ((x)-'0')
+#define int ll
+#define double long double
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<vi> vvi;
+
+int dx[] = { -2, -2, -1, -1,  1, 1,  2, 2 };
+int dy[] = { -1,  1, -2,  2, -2, 2, -1, 1 };
+
+int n, k;
+
+signed main() {
+	cin >> n >> k;
+	if (n >= k * (k + 1) / 2) {
+		if ((n - (k * (k + 1) / 2)) % k == 0) {
+			cout << k - 1;
+		}
+		else {
+			cout << k;
+		}
+	}
+	else {
+		cout << -1;
+	}
+}
