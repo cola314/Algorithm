c3c105016037b3e20b5f16076fb02d94365e19cd
cola <66579357+cola314@users.noreply.github.com>
2023-02-04 14:15:11 +0900

    [BOJ] 1302번 베스트셀러

---
+++ BOJ/1302.cpp
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+#define fastio cin.tie(NULL); cin.sync_with_stdio(false);
+#define endl "\n"
+#define FOR(x, y) for(int x = 0; x < y; x++)
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define sz(x) ((int)x.size())
+#define ini(x, y) memset(x, y, sizeof(x));
+#define INF 987654321
+#define y1 omg
+#define fi first
+#define se second
+#define mp make_pair
+#define ceildv(x, y) ((x) / (y) + ((x) % (y) != 0))
+#define sumn(x) ((x) * ((x) + 1) / 2)
+#define square(x) ((x) * (x))
+#define int ll
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using ii = pair<int, int>;
+using iii = pair<pair<int, int>, int>;
+using vii = vector<ii>;
+
+int n, maxv, cur;
+string s, rs;
+vector<string> v;
+
+signed main() {
+    cin >> n;
+    FOR(i, n) {
+        cin >> s;
+        v.pb(s);
+    }
+    sort(all(v));
+
+    FOR(i, sz(v)) {
+        if (i == 0 || v[i] == v[i - 1]) {
+            cur++;
+        } else {
+            cur = 1;
+        }
+        if (cur > maxv) {
+            maxv = cur;
+            rs = v[i];
+        }
+    }
+
+    cout << rs;
+    
+    return 0;
+}
